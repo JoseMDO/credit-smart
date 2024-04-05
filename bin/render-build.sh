94134e3be98078fff8e3c4394d50bfb785762ec7
Jose Montes De Oca Morfin <112668683+JoseMDO@users.noreply.github.com>
2024-04-05 22:57:38 +0000

    added slurp tasks to render build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,3 +8,6 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+bundle exec rake slurp:categories
+bundle exec rake slurp:credit_cards
+bundle exec rake slurp:rewards
