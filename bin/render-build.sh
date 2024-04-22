be8c11a6a516e6c3e2abe24c97f300dc9d2de013
JoseMDO <joseamontesdeocamo@lewisu.edu>
2024-04-22 13:15:33 -0500

    another fix to fix asset pipeline issue

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,7 +7,6 @@ bundle install
 # For Ruby on Rails apps uncomment these lines to precompile assets and migrate your database.
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake assets:clobber
 bundle exec rake db:migrate
 
 bundle exec rake slurp:categories
